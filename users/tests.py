0c6062005e0f7f60e08a790c357391d0e212aad3
BuildTools <unconfigured@null.spigotmc.org>
2021-09-26 18:05:47 -0700

    create seperate file for navbar

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import AppUser
+from .forms import AppUserCreationForm
 
-# Create your tests here.
+def setUp(self):
+    # Setup run before every test method.
+    pass
+
+
+def tearDown(self):
+    # Clean up run after every test method.
+    pass
+
+
+def test_something_that_will_pass(self):
+    self.assertFalse(False)
+
+
+def test_something_that_will_fail(self):
+    self.assertTrue(False)
