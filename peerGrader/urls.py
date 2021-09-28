68cf7ed8b3a8ed0883730e42e08aa70de5e1697b
BuildTools <andre.domingo@sjsu.edu>
2021-09-28 01:31:25 -0700

    split homepages into separate files

--- peerGrader/urls.py
+++ peerGrader/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+import users.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', users.views.homeView, name='home'),
     path('users/', include(('users.urls', 'users'))),
     path('users/', include('django.contrib.auth.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -8,4 +8,12 @@ from .forms import AppUserCreationForm
 class SignUpView(CreateView):
     form_class = AppUserCreationForm
     success_url = reverse_lazy('home')
-    template_name = 'users/signup.html'
+    template_name = 'registration/signup.html'
+
+
+def homeView(request):
+    if not request.user.is_authenticated:
+        return render(request, 'home.html')
+    if request.user.is_instructor:
+        return render(request, 'users/instructorhome.html')
+    return render(request, 'users/studenthome.html')
