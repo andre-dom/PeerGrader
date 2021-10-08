ab317c5540640dc46b4fc002068360620fa07a79
BuildTools <andre.domingo@sjsu.edu>
2021-10-07 19:13:42 -0700

    added assignment deletion

--- assignments/urls.py
+++ assignments/urls.py
@@ -6,4 +6,5 @@ app_name = "assignments"
 urlpatterns = [
     path('<slug:course_slug>/new/', assignments.views.assignment_create_view, name='create_assignment'),
     path('<slug:slug>/', assignments.views.assignment_detail_view, name='view_assignment'),
+    path('<slug:slug>/delete/', assignments.views.assignment_delete_view, name='delete_assignment'),
 ]
--- assignments/views.py
+++ assignments/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, DeleteView
 
 from assignments.models import Assignment
 from courses.models import Course
@@ -27,6 +27,14 @@ class CreateAssignment(CreateView):
         form.instance.course = Course.objects.get(slug=self.kwargs['course_slug'])
         return super(CreateAssignment, self).form_valid(form)
 
+class DeleteAssignment(DeleteView):
+    model = Assignment
+    slug_url_kwarg = 'slug'
+    slug_field = 'slug'
+    success_url = "/"
+    template_name = 'assignments/deleteassignment.html'
+
 
 assignment_detail_view = login_required(AssignmentView.as_view())
 assignment_create_view = login_required(CreateAssignment.as_view())
+assignment_delete_view = login_required(DeleteAssignment.as_view())
