6342bebba06b1f727d0a3dd14000ecc0bf0292fa
BuildTools <andre.domingo@sjsu.edu>
2021-10-14 16:36:14 -0700

    split course view into seperate student and instructor templates

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -9,6 +10,12 @@ from courses.models import Course
 @login_required()
 def course_view(request, slug):
     course = Course.objects.get(slug=slug)
+    user = request.user
+
+    # Make sure authenticated user is part of the course
+    if not (course.instructor == user or (user in course.students.all())):
+        return redirect('/')
+
     assignment_list = course.assignments.values()
 
     page = request.GET.get('page', 1)
@@ -20,7 +27,10 @@ def course_view(request, slug):
     except EmptyPage:
         assignments = paginator.page(paginator.num_pages)
 
-    return render(request, 'courses/courseview.html', {'course': course, 'assignments': assignments, })
+    if request.user.is_instructor:
+        return render(request, 'courses/instructorcourseview.html', {'course': course, 'assignments': assignments, })
+    else:
+        return render(request, 'courses/studentcourseview.html', {'course': course, 'assignments': assignments, })
 
 
 course_detail_view = login_required(course_view)
